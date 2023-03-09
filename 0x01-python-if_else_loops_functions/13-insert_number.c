73ce022081f9db2ade0d66e009e1af1651feff62
Eruoti1 <kevinombui49@gmail.com>
2023-03-08 18:50:34 -0800

    Write a function in C that inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,42 @@
-/*
- * File: 13-insert_number.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 
-/**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the new node.
- */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+listint_t *current, *new;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+if (head == NULL)
+return (NULL);
+new = malloc(sizeof(listint_t));
 
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
+if (new == NULL)
+return (NULL);
+new->n = number;
+new->next = NULL;
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+current = *head;
+if (current->n > new->n)
+{
+new->next = current;
+*head = new;
+return (new);
+}
 
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+while(current && current->next)
+{
+if (current->next->n < new->n)
+current = current->next;
+else
+{
+new->next = current->next;
+current->next = new;
+return (new);
+}
+current->next = new;
+return (new);
+}
 }
