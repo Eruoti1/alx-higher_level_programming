bd5ec3bd9cf9ea512699dba9f6f5b9903534033d
Eruoti1 <kevinombui49@gmail.com>
2023-03-09 18:05:42 -0800

    rogram that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    import sys
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("{:d} arguments.".format(num_args))
+    elif num_args == 1:
+        print("{:d} argument:".format(num_args))
+    else:
+        print("{:d} arguments:".format(num_args))
+
+    if num_args >= 1:
+        num_args = 0
+        for arg in sys.argv:
+            if num_args != 0:
+                print("{:d}: {:s}".format(num_args, arg))
+            num_args += 1
